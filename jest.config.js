e32f6e18f6144d17107ba373af9dbe4566884102
shayan mudassar <shynmudassar@gmail.com>
2026-02-21 14:14:54 +0000

    test: use v8 coverage provider

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/web/'],
+  coverageProvider: 'v8',
   moduleNameMapper: {
     '^@sentinel/(.*)$': '<rootDir>/libs/$1/src'
   }
